199475cf93247abc02756effba7a4eb3eb8ab408
deviousprophet <69911034+deviousprophet@users.noreply.github.com>
2020-08-31 13:19:25 +0700

    commit

--- esp-idf/template-app/main/main.c
+++ esp-idf/template-app/main/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_wifi.h"
 
 void app_main(void)
 {
     printf("Hello world!\n");
+
     for (int i = 10; i >= 0; i--) {
-        printf("Stop in %d seconds...\n", i);
+        printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-    printf("Stopped.\n");
+    printf("Restarting now.\n");
+    esp_restart();
 }
