5a2411c98a401e764d892057ce96a0d2ffb56e9d
deviousprophet <69911034+deviousprophet@users.noreply.github.com>
2020-09-02 21:57:23 +0700

    commit

--- MQTT_Service.py
+++ MQTT_Service.py
@@ -1,17 +1,30 @@
 import paho.mqtt.client as mqtt
 
 broker_address = "192.168.137.1"
+subTopic = "hotel/+/relay"
+hotel = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 
 def on_message(client, userdata, message):
+    global hotel
     _topic = str(message.topic)
-    _value = int(str(message.payload.decode("utf-8")))
+    _data = int(str(message.payload.decode("utf-8")))
+
+    start = "hotel/"
+    end = "/relay"
+    room = int(_topic[len(start):-len(end)])
+    
+    floor = room%100
+    room_floor = int(room/100)
+
+    hotel[room_floor-1][floor-1] = _data
+    print(hotel)
 
 def on_disconnect (client, userdata, rc):
     print("Disconnected")
 
 def on_connect (client, obj, flags, rc):
     print("Connected")
-    client.subscribe("hotel/+/relay")
+    client.subscribe(subTopic)
 
 client = mqtt.Client()
 client.on_message=on_message
